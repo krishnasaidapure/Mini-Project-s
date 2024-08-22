7d561065a3954d7a8089a628af839321fa03cd17
Krishna Saidapure <krishnasaidapure12@gmail.com>
2024-08-23 01:56:59 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let rect = document.querySelector("#rect");
-
-rect.addEventListener("mousemove", function(blg){
-    let val = gsap.utils.mapRange(0, window.innerWidth, 72, window.innerWidth-148+rect.getBoundingClientRect().width/2, blg.clientX)
-    gsap.to('#rect',{
-        left: val + "px",
-        ease: Power3
-    });
-});
